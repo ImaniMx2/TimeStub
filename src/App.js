1d9e3f54b203cf637ac66546d470bdf0c1017191
nzaya1 <themuias1@gmail.com>
2023-04-16 10:24:29 -0400

    changeed app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          TimeStub<code>Hello</code> and save to reload.
+          TimeStub<code>Hello-me</code> Nzaya muia.
         </p>
         <a
           className="TimeStub"
